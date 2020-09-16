64cf9e3c7c9721b20da41a0ffba938bda68cad58
evaEZ <eva.jingzhao@gmail.com>
2020-09-16 13:08:33 +0000

    Some important context here

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+//Function IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2){
+  let output = true;
+  if (arr1.length === arr2.length){
+    for (let i = 0; i < arr1.length; i++){
+      if (arr1[i] !== arr2[i]) {
+        output = false;
+        break;
+      }
+    }
+  } else {
+    output = false;
+  }
+  return output;
+};
+
+//TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);

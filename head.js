85f076f0130aace12c32063c9167d61dc1806633
evaEZ <eva.jingzhao@gmail.com>
2020-09-16 01:57:43 +0000

    Some important context here

---
+++ head.js
@@ -0,0 +1,22 @@
+//Function IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function (arr){
+  if (arr === undefined){
+    return undefined;
+  } else {
+    return arr[0];
+  }
+};
+
+//TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([3]),3);
+assertEqual(head(),undefined);

b34d24dc642021d06e9dcb8cc7f288d4d2e80588
evaEZ <eva.jingzhao@gmail.com>
2020-09-17 00:03:49 +0000

    Some important context here

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+//Function IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, value){
+  let result;
+  for (const items in obj){
+    if (obj[items] === value){
+      result = items;
+    }
+  }
+  if(result){
+    return result;
+  } else {
+    return undefined;
+  }
+}
+//TEST CODE
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

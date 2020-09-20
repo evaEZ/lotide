4346a31397b0f29049ae41e61a185de4dfe1708a
evaEZ <eva.jingzhao@gmail.com>
2020-09-20 16:22:34 +0000

    Some important context here

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,4 @@
+/*
 const assertArraysEqual = require ('../assertArraysEqual');
 const middle = require ('../middle');
 
@@ -7,3 +8,33 @@ assertArraysEqual(middle([1,2,3]), [2]);
 assertArraysEqual(middle([1,2,3,4,5]), [3]);
 assertArraysEqual(middle([1,2,3,4]), [2,3]);
 assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
+*/
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#head", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns '[]' for [1,2]", () => {
+    assert.deepEqual(middle([1,2]), []); 
+  });  
+
+  it("returns '[2]' for [1,2]", () => {
+    assert.deepEqual(middle([1,2,3]), [2]); 
+  }); 
+
+  it("returns '[1,2,3,4,5]' for [3]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]); 
+  }); 
+
+  it("returns '[1,2,3,4]' for [2,3]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  }); 
+
+  it("returns '[1,2,3,4,5,6]' for [3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]); 
+  }); 
+});

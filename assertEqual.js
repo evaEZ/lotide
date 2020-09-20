fe41050e47af101fd2f070d39307921e073e2b47
evaEZ <eva.jingzhao@gmail.com>
2020-09-20 14:51:50 +0000

    Some important context here

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertEqual;
+
+/*
 //TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1,1);
 assertEqual("Happy Friday", "Happy Friday");
 assertEqual(11, 3);
+*/
